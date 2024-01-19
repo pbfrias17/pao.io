c5e07753641653263f2cf3ce3bb1e50561f5784c
pbfrias17 <pbfrias17@gmail.com>
2024-01-19 13:47:49 -0500

    rename and clean seed data

---
+++ src/data/paoDBData.js
@@ -0,0 +1,10 @@
+const paolo = [
+    { letter: 'p', words: ['planner of sprints', 'personable person', 'pair-programmable', 'poly-programming linguistics', 'pharmacuedically-dependent']},
+    { letter: 'a', words: ['awesome', 'artificially intelligent', 'armed with desire to grow', 'abs']},
+    { letter: 'o', words: ['O(n)', 'optimal', 'omnivorous', 'olive-hater', 'omniscient', 'optically-impaired']},
+    { letter: 'l', words: ['low-latency', 'literate', 'lazy-loading', 'LLM', 'long-sighted', 'legally able to work in US', '1337']},
+]
+  
+module.exports = {
+    paolo,
+};
